048d2c63a90957ee6c928c83ff63d46b51f29617
ZaidSultan10 <zaidsultan09@gmail.com>
2022-08-15 00:44:42 +0400

    making select for leftsidebar

--- src/components/optionsList/OptionsList.jsx
+++ src/components/optionsList/OptionsList.jsx
@@ -3,28 +3,35 @@ import './_optionsList.scss'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { useEffect } from 'react'
 
-const OptionsList = ({option,icon,count, color,key,id}) => {
+const OptionsList = ({option,icon,count, color,id}) => {
 
     const [selectOption,setSelectOption] = useState(false)
-    const [initialSelectedOption,setInitialSelectedOption] = useState('Dashboard')
+    const [initialSelectedOption,setInitialSelectedOption] = useState('')
+    const [theArray, setTheArray] = useState([]);
 
     const handleSelectOptionChange = () => {
-        console.log('option -->',option)
-        setInitialSelectedOption(option)
+        // setInitialSelectedOption(option)
+        // if(theArray.length > 0){
+        //     setTheArray([])
+        // }
+        setTheArray([...theArray,option])
+        console.log('id -->',initialSelectedOption,'option -->',option)
     }
 
     // useEffect(() => {
-    //     console.log('option -->',option,'initialSelectedOption -->',initialSelectedOption)
-    //     if(option == initialSelectedOption){
-    //         setSelectOption(true)
-    //     }else{
-    //         setSelectOption(false)
-    //     }
-    // },[initialSelectedOption])
-    // console.log('option -->',option === initialSelectedOption ? option : '','initialSelectedOption -->',initialSelectedOption)
+    //     // if(option === initialSelectedOption){
+    //     //     setSelectOption(true)
+    //     // }else{
+    //     //     setSelectOption(false)
+    //     // }
+    //     console.log('option -->',theArray)
+    // },[theArray])
+
+    // console.log('option -->',theArray)
 
   return (
-    <div onClick={handleSelectOptionChange} className={option == initialSelectedOption ? `optionsList optionsList__active` : option !== initialSelectedOption ? `optionsList` : `optionsList`}>
+    console.log('theArray.includes(option) -->',theArray.includes(option)),
+    <div key={id} onClick={handleSelectOptionChange} className={`optionsList ${theArray.includes(option) ? `optionsList__active` : ''}`}>
         <div className='optionsList__left'>
             <FontAwesomeIcon style={{color:color}} icon={icon} />
             <p>{option}</p>
