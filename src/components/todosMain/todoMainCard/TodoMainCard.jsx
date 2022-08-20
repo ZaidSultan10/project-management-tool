e4236c477113bc453d6cb0e7fa5621a18b644631
ZaidSultan10 <zaidsultan09@gmail.com>
2022-08-21 00:38:47 +0400

    task card design update

--- src/components/todosMain/todoMainCard/TodoMainCard.jsx
+++ src/components/todosMain/todoMainCard/TodoMainCard.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './_todoMainCard.scss'
 import Header from '../../header/Header.jsx'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEllipsisH,faPlus} from '@fortawesome/fontawesome-free-solid'
+import {faEllipsisH,faPlus,faChevronCircleLeft,faChevronCircleRight} from '@fortawesome/fontawesome-free-solid'
 import TodoMainTaskCard from './todoMainTaskCard/TodoMainTaskCard'
 
 const TodoMainCard = ({headingTitle, count}) => {
+
+    const [paginationCount, setPaginationCount] = useState(1)
+
   return (
     <div className='todoMainCard'>
         <div className='todoMainCard__heading'>
@@ -25,11 +28,34 @@ const TodoMainCard = ({headingTitle, count}) => {
             <TodoMainTaskCard />
         </div>
         <div className='todoMainCard__footer'>
-            <button>
-               <span>
-                <FontAwesomeIcon style={{marginRight:'10px'}} color='gray' icon={faPlus} />
-               </span> {`Add Task`}
-            </button>
+            <div className='todoMainCard__footer__left'>
+                <button>
+                    <span>
+                        <FontAwesomeIcon style={{marginRight:'10px'}} color='gray' icon={faPlus} />
+                    </span> {`Add Task`}
+                </button>
+            </div>
+            <div className='todoMainCard__footer__right'>
+                <div className='todoMainCard__footer__right__prev'>
+                    <FontAwesomeIcon color='gray' style={{fontSize:'22px'}} icon={faChevronCircleLeft} />
+                </div>
+                <div className='todoMainCard__footer__right__start'>
+                    <div style={{backgroundColor:'gray',borderRadius:'50%',width:'22px',height:'22px', display:'flex',justifyContent:'center',alignItems:'center'}} className='todoMainCard__footer__right__start__style'>
+                        <p style={{fontSize:'10px'}}>{paginationCount}</p>
+                    </div>
+                </div>
+                <div className='todoMainCard__footer__right__truncate'>
+                    <h3>{`. . .`}</h3>
+                </div>
+                <div style={{backgroundColor:'gray',borderRadius:'50%',width:'22px',height:'22px', display:'flex',justifyContent:'center',alignItems:'center'}} className='todoMainCard__footer__right__end'>
+                    <div className='todoMainCard__footer__right__end__style'>
+                        <p style={{fontSize:'10px'}}>10</p>
+                    </div>
+                </div>
+                <div className='todoMainCard__footer__right__next'>
+                    <FontAwesomeIcon color='gray' style={{fontSize:'22px'}} icon={faChevronCircleRight} />
+                </div>
+            </div>
         </div>
     </div>
   )
