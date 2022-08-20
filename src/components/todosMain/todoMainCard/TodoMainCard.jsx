d3a001c35bf187c7e588f4d28381530682ce06cf
ZaidSultan10 <zaidsultan09@gmail.com>
2022-08-21 02:31:27 +0400

    task card design update

--- src/components/todosMain/todoMainCard/TodoMainCard.jsx
+++ src/components/todosMain/todoMainCard/TodoMainCard.jsx
@@ -37,23 +37,23 @@ const TodoMainCard = ({headingTitle, count}) => {
             </div>
             <div className='todoMainCard__footer__right'>
                 <div className='todoMainCard__footer__right__prev'>
-                    <FontAwesomeIcon color='gray' style={{fontSize:'22px'}} icon={faChevronCircleLeft} />
+                    <FontAwesomeIcon color='#d5d5d5' style={{fontSize:'22px'}} icon={faChevronCircleLeft} />
                 </div>
                 <div className='todoMainCard__footer__right__start'>
-                    <div style={{backgroundColor:'gray',borderRadius:'50%',width:'22px',height:'22px', display:'flex',justifyContent:'center',alignItems:'center'}} className='todoMainCard__footer__right__start__style'>
-                        <p style={{fontSize:'10px'}}>{paginationCount}</p>
+                    <div style={{backgroundColor:'#d5d5d5',borderRadius:'50%',width:'22px',height:'22px', display:'flex',justifyContent:'center',alignItems:'center'}} className='todoMainCard__footer__right__start__style'>
+                        <p style={{fontSize:'10px',fontWeight:'bold',color:'gray'}}>{paginationCount}</p>
                     </div>
                 </div>
                 <div className='todoMainCard__footer__right__truncate'>
                     <h3>{`. . .`}</h3>
                 </div>
-                <div style={{backgroundColor:'gray',borderRadius:'50%',width:'22px',height:'22px', display:'flex',justifyContent:'center',alignItems:'center'}} className='todoMainCard__footer__right__end'>
+                <div style={{backgroundColor:'#d5d5d5',borderRadius:'50%',width:'22px',height:'22px', display:'flex',justifyContent:'center',alignItems:'center'}} className='todoMainCard__footer__right__end'>
                     <div className='todoMainCard__footer__right__end__style'>
-                        <p style={{fontSize:'10px'}}>10</p>
+                        <p style={{fontSize:'10px',fontWeight:'bold',color:'gray'}}>10</p>
                     </div>
                 </div>
                 <div className='todoMainCard__footer__right__next'>
-                    <FontAwesomeIcon color='gray' style={{fontSize:'22px'}} icon={faChevronCircleRight} />
+                    <FontAwesomeIcon color='#d5d5d5' style={{fontSize:'22px'}} icon={faChevronCircleRight} />
                 </div>
             </div>
         </div>
