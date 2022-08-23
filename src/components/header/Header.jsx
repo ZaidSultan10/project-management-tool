4acf98790f6f9621415b78642c2788c8b900e5a3
ZaidSultan10 <zaidsultan09@gmail.com>
2022-08-23 23:25:56 +0400

    making add task modal

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Header = ({title,fontSize,marginLeft}) => {
+const Header = ({title,fontSize,marginLeft,textAlign}) => {
   return (
     <div>
-        <h1 style={{fontSize:fontSize, marginLeft:marginLeft}}>{title}</h1>
+        <h1 style={{fontSize:fontSize, marginLeft:marginLeft, textAlign:textAlign}}>{title}</h1>
     </div>
   )
 }
--- src/components/todosMain/addTodoModal/AddTodoModal.jsx
+++ src/components/todosMain/addTodoModal/AddTodoModal.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Header from '../../header/Header'
+import './_addTodoModal.scss'
 
-const AddTodoModal = () => {
+const AddTodoModal = ({isEdit,handleChange}) => {
+
   return (
-    <div>AddTodoModal</div>
+    <div className='addTodoModal'>
+        <div className='addTodoModal__header'>
+            <Header title={isEdit ? `Edit Task` : `Add Task`} fontSize={`18px`} textAlign={`center`} />
+        </div>
+        <div className='addTodoModal__taskTitle'>
+            <label htmlFor='taskTitleId'>
+                <Header title={`Task Title`} fontSize={`16px`} />
+            </label>
+            <input className='addTodoModal__taskTitle__input' id='taskTitleId' onChange={e => handleChange(e)} />
+        </div>
+    </div>
   )
 }
 
