112e734bd2dbbeb98e9cdd4ae6763dcb57ed7dee
ZaidSultan10 <zaidsultan09@gmail.com>
2022-08-30 23:53:38 +0400

    redux setup done

--- src/reducers/task.js
+++ src/reducers/task.js
@@ -1,4 +1,4 @@
-import {GET_ALL_TODOS,CREATE_TODO,DELETE_TODO,UPDATE_STATUS,UPDATE_TODO} from '../actions/types.js'
+// import {GET_ALL_TODOS,CREATE_TODO,DELETE_TODO,UPDATE_STATUS,UPDATE_TODO} from '../actions/types.js'
 
 let initialValue = {
     todos: [],
